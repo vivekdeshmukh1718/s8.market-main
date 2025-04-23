59152662ec6a6492b741b71990578ca769350b87
vivek-deshmukh <143309339+vivekdeshmukh1718@users.noreply.github.com>
2025-04-23 10:40:25 +0530

    Update main.jsx

--- BankOfficer/src/main.jsx
+++ BankOfficer/src/main.jsx
@@ -1,17 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import AppContextProvider from "./context/context.jsx";
-import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
+const rootElement = document.getElementById("root");
 
-createRoot(document.getElementById("root")).render(
+createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </BrowserRouter>
-  </StrictMode> // REMOVE AFTER DEPLOYMENT
+  </StrictMode> // Optional: Remove StrictMode for production if needed
 );
+
